ff3510868da377a7fd6eabad6b8ddb9967cd6daa
olanrewaju Makinde <lanre.developer@gmail.com>
2020-04-27 09:56:38 +0100

    setup boiler plate code for main container

---
+++ src/containers/MainContainer.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import GameContainer from "./GameContainer.jsx";
+
+const mapStateToProps = (state) => {
+  return {
+    gameBoardSize: state.gameBoardSize,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  // dispatch the action of board size selection
+};
+
+class MainContainer extends Component {
+  constructor(props) {
+    super(props);
+    // console.log(props);
+    // console.log(this.props);
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <GameContainer />
+        {/* <h1 id="header">MegaMarket Loyalty Cards</h1> */}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
