25aad0d90a2ef0d45e6aec59380abb7936e940a9
olanrewaju Makinde <lanre.developer@gmail.com>
2020-04-27 09:48:27 +0100

    merged reducers into combineReducers for redux

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import { gameReducers } from "./gameReducers";
+
+const reducers = combineReducers({
+  // if we had other reducers, they would go here
+  game: gameReducers,
+});
+
+// make the combined reducers available for import
+export default reducers;
