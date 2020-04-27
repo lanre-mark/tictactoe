b52a75bd2c5baf3b809fa4bb88ae4ff54d29ad9f
olanrewaju Makinde <lanre.developer@gmail.com>
2020-04-27 11:35:11 +0100

    added BoardSize component

---
+++ src/component/BoardSize.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const BoardSize = (props) => {
+  return (
+    <div id="boardsizecontrol">
+      <div class="keys">
+        <div data-key="65" class="key__button">
+          3
+        </div>
+        <div data-key="65" class="key__button">
+          5
+        </div>
+        <div data-key="65" class="key__button">
+          7
+        </div>
+        <div data-key="65" class="key__button">
+          9
+        </div>
+        <div data-key="65" class="key__button">
+          11
+        </div>
+        <div data-key="65" class="key__button">
+          13
+        </div>
+        <div data-key="65" class="key__button">
+          15
+        </div>
+        <div data-key="65" class="key__button">
+          17
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BoardSize;
