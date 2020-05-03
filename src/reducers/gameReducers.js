1e2bf27f5321dc9c25a7ad2d3dbc1ba6a4e1b815
olanrewaju Makinde <lanre.developer@gmail.com>
2020-05-03 13:21:25 +0100

    completed BOARD_SIZE_SELECTION reducer

--- src/reducers/gameReducers.js
+++ src/reducers/gameReducers.js
@@ -9,21 +9,39 @@ const initialState = {
 };
 
 const gameReducers = (state = initialState, action) => {
-  if (action.type === types.TICK_BOARD) {
-    return state;
-  }
-  if (action.type === types.SELECT_BOARD_SIZE) {
-    return state;
-  }
-  return state;
-  // switch (action.type) {
-  //   case types.TICK_BOARD:
-  //     return state;
-  //   case types.SELECT_BOARD_SIZE:
-  //     return state;
-  //   case default:
-  //     return state;
+  // if (action.type === types.TICK_BOARD) {
+  //   return state;
   // }
+  // if (action.type === types.SELECT_BOARD_SIZE) {
+  //   console.log("selected size of board");
+  //   // return a brand new state
+  //   return {
+  //     ...state,
+  //     gameOver: false,
+  //     drawGame: false,
+  //     currentPlayer: "x",
+  //     spaces: [...Array(action.payload ** 2).keys()].map((d) => null),
+  //     size: action.payload,
+  //   };
+  // }
+  // return state;
+  switch (action.type) {
+    case types.TICK_BOARD:
+      return state;
+    case types.SELECT_BOARD_SIZE:
+      console.log("selected size of board");
+      // return a brand new state
+      return {
+        ...state,
+        gameOver: false,
+        drawGame: false,
+        currentPlayer: "x",
+        spaces: [...Array(action.payload ** 2).keys()].map((d) => null),
+        size: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default gameReducers;
