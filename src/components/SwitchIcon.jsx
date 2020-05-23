ac449a3519f14d088ee196e02fefa716b0111961
olanrewaju Makinde <lanre.developer@gmail.com>
2020-05-24 00:12:03 +0100

    refined perserve state switch button

--- src/components/SwitchIcon.jsx
+++ src/components/SwitchIcon.jsx
@@ -1,50 +1,51 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const shrinkTransitions = keyframes`
+                                    "0%": {
+                                      transform: "scale(1)"
+                                    },
+                                    "33%": {
+                                      transform: "scale(0.85)"
+                                    },
+                                    "100%": {
+                                      transform: "scale(1)"
+                                    }
+                                  `;
+
+const StatePreservationDescription = styled.label`
+  position: "relative",
+  display: "flex",
+  margin: "0.6em 0",
+  alignItems: "center",
+  color: "#9e9e9e",
+  font-size: 14px;
+  transition: "color 250ms cubic-bezier(0.4, 0, 0.23, 1)",
+  "&::after": {
+    content: "''",
+  },
+  "&::before": {
+    content: "''",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: "1em",
+    width: "1em",
+    height: "1em",
+    background: "transparent",
+    boxShadow: "0 0 0 2px #9e9e9e inset",
+    borderRadius: "50%",
+    cursor: "pointer",
+    transition: "all 250ms cubic-bezier(0.4, 0, 0.23, 1)",
+  },
+`;
 
 const PreserveSwitchIcon = (props) => {
   return (
     <div className="preserve__icon">
-      {props.switch === true && (
-        <svg
-          className="switch"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 51.6 51.6"
-          style={{ enableBackground: "new 0 0 51.6 51.6" }}
-          xmlSpace="preserve"
-        >
-          <line
-            className="preserve__line"
-            x1="10.4"
-            y1="10"
-            x2="41.2"
-            y2="41.6"
-          />
-          <line
-            className="preserve__line"
-            x1="41.6"
-            y1="10.4"
-            x2="10"
-            y2="41.2"
-          />
-        </svg>
-      )}
-
-      {props.switch !== true && (
-        <svg
-          className="preserve"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 60 60"
-          style={{ enableBackground: "new 0 0 60 60" }}
-          xmlSpace="preserve"
-        >
-          <circle className="preserve__line" cx="30" cy="30" r="20" />
-        </svg>
-      )}
+      <StatePreservationDescription>
+        {props.switch === true ? "ON" : "OFF"}
+      </StatePreservationDescription>
     </div>
   );
 };
